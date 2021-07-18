ae6a1113341c02ecd2ef970b829a6a3d76fbb9d0
DESKTOP-DCILK67\97081 <0816yw@gmail.com>
2021-07-18 19:40:06 +0900

    기본형 매개변수와 참조형 매개변수 차이점 구분하기

--- 06_Object/src/method/model/Method.java
+++ 06_Object/src/method/model/Method.java
@@ -1,5 +1,19 @@
 package method.model;
 
 public class Method {
+	
+	// 기본형 매개변수와 참조형 매개변수 구분하기
+	
 
+	public int method1(int num1, int num2) {  // 기본자료형
+		num1++;
+		num2++;
+		System.out.println("메소드 num1: " + num1 + ", num2: " + num2);
+		return num1+num2;
+	}
+	
+	public int[] method2(int[]arr) {
+		arr[2] = 99;
+		return arr;
+	}
 }
--- 06_Object/src/method/run/Run.java
+++ 06_Object/src/method/run/Run.java
@@ -1,10 +1,26 @@
 package method.run;
 
+import java.util.Arrays;
+
+import method.model.Method;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Method m = new Method();
+		
+		int num1 = 3;
+		int num2 = 5;
+		int result = m.method1(num1, num2);
+		System.out.println("main num1: " + num1 + ", num2: " + num2 );
+		System.out.println("result : " + result);
+		
+		
+		int[] arr = {1,2,3,4,5};
+		int[] returnarr = m.method2(arr);
+		System.out.println("arr : " + Arrays.toString(arr));
+		System.out.println("returnarr : " + Arrays.toString(returnarr));
 	}
 
 }
