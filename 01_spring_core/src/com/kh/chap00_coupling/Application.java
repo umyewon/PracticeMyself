c6e947aa3f9fd96e894633e4e1745c356ff1d4d6
umyewon <0816yw@gmail.com>
2021-11-30 23:29:31 +0900

    spring test

--- 01_spring_core/src/com/kh/chap00_coupling/Application.java
+++ 01_spring_core/src/com/kh/chap00_coupling/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// new ClassB(new ClassAA()).methodB();
-		
+		 
 		/* setter를 사용 */
 		ClassB classB = new ClassB();
 		classB.setClassA(new ClassAAA());
