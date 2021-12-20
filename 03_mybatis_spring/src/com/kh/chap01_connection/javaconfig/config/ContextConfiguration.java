a5f70c5365eaba93c5093c89bfbf7d4e5bd78023
umyewon <0816yw@gmail.com>
2021-12-21 00:29:22 +0900

    mybatis_spring 복습

--- 03_mybatis_spring/src/com/kh/chap01_connection/javaconfig/config/ContextConfiguration.java
+++ 03_mybatis_spring/src/com/kh/chap01_connection/javaconfig/config/ContextConfiguration.java
@@ -16,8 +16,9 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("connection-info.properties")	// DB연결정보 프로퍼티 (아웃풋 폴더 최상단에 위치)
 public class ContextConfiguration {
 
-	@Value("${oracle.dev.driver}")
-	private String driver;			// oracle.dev.driver가 가지고 있는 value값을 필드값(String driver)에 담음
+	/* DB 연결정보 */
+ 	@Value("${oracle.dev.driver}")	// 프로퍼티 키 값 (키 별로 각각 작성)
+	private String driver;			// oracle.dev.driver 가 가지고 있는 value값을 필드값(String driver)에 담음
 	@Value("${oracle.dev.url}")
 	private String url;
 	@Value("${oracle.dev.username}")
@@ -26,16 +27,18 @@ public class ContextConfiguration {
 	private String password;
 	
 	
+	// My Batis 연동을 위한 빈 등록
 	/* commons-dbcp(Database Connection Pooling) 라이브러리 추가  (요즘 사용하는 hikari cp) */
 	/* commons-pool은 commons-dbcp가 사용하는 라이브러리임으로 추가 */
 	@Bean(destroyMethod="close")			// 폐기 메소드로 close 정의하여 객체 반납 자동으로 처리
 	public BasicDataSource dataSource() {   // 스프링에서 DB연동 모듈 사용하기 위한 객체
 		BasicDataSource dataSource = new BasicDataSource();
 		
-		dataSource.setDriverClassName(driver);		// 필드에서 선언한 driver 담기
-		dataSource.setUrl(url);						// 필드에서 선언한 url 담기
-		dataSource.setUsername(username);			// 필드에서 선언한 username 담기
-		dataSource.setPassword(password);			// 필드에서 선언한 password 담기
+		// BasicDataSource에 필드에서 선언한 DB정보 담기
+		dataSource.setDriverClassName(driver);		
+		dataSource.setUrl(url);						
+		dataSource.setUsername(username);			
+		dataSource.setPassword(password);			
 		dataSource.setDefaultAutoCommit(false);		// 수동 커밋을 위해 오토커밋 막기
 		
 		return dataSource;
@@ -45,12 +48,12 @@ public class ContextConfiguration {
 	public SqlSessionFactory sqlSessionFactory(ApplicationContext context) throws Exception {	// 실행 클래스(Application)에서 선언한 것 사용
 		
 		/* SqlSessionFactoryBean은 mybatis-spring 라이브러리 추가 필요*/
-		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();		// SqlSessionFactoryBean을 사용해서 SqlSessionFactory 생성
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();	// SqlSessionFactoryBean을 사용해서 SqlSessionFactory 생성
 		
 		/* 위에서 등록한 dataSource bean 설정 - setter주입 */
 		factoryBean.setDataSource(dataSource());
 		
-		/* mybatis config 파일 설정 : ApplicationContext 객체의 getResource 메소드 사용해서 설정 파일 주입 */
+		/* mybatis config 파일 설정 : 리소스 타입으로 설정 해줘야되기 때문에 ApplicationContext 객체의 getResource 메소드 사용해서 설정 파일 주입 */
 		factoryBean.setConfigLocation(context.getResource("com/kh/chap01_connection/javaconfig/config/mybatis-config.xml"));
 		
 		/* spring-jdbc 라이브러리 추가 필요*/
