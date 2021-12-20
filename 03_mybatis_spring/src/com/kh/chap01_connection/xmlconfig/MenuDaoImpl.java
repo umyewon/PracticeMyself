ef0dc2893ec3cecc673ae5890311c92b09dca961
umyewon <0816yw@gmail.com>
2021-12-21 00:46:08 +0900

    mybatis_spring 복습

--- 03_mybatis_spring/src/com/kh/chap01_connection/xmlconfig/MenuDaoImpl.java
+++ 03_mybatis_spring/src/com/kh/chap01_connection/xmlconfig/MenuDaoImpl.java
@@ -10,6 +10,7 @@ public class MenuDaoImpl implements MenuDao{ // MenuDao 실 구현 클래스
 
 	@Override
 	public List<Menu> selectMenuList(SqlSessionTemplate sqlSession) {
+		// SqlSessionTemplate도 이전에 사용하던 SqlSession객체와 동일하게 메소드 사용 가능
 		return sqlSession.selectList("MenuMapper.selectMenuList");		// namespace.id
 	}
 
--- 03_mybatis_spring/src/com/kh/chap01_connection/xmlconfig/MenuServiceImpl.java
+++ 03_mybatis_spring/src/com/kh/chap01_connection/xmlconfig/MenuServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class MenuServiceImpl implements MenuService{ // MenuService 실 구현 클래스
 
 	private final MenuDao menuDao;		// final로 선언했기 때문에 초기화 필요 -> new MenuDao하지 않고 @Autowired (생성자 주입 방식으로)
-	private final SqlSessionTemplate sqlSession;		// DB와의 연결 처리를 위해 선언 (configuration을 통해 스프링 컨테이너가 가지고 있기 때문에 생성자에 추가하면 됨)
+	private final SqlSessionTemplate sqlSession;	// DB와의 연결 처리를 위해 선언 (configuration을 통해 스프링 컨테이너가 가지고 있기 때문에 생성자에 추가하면 됨)
 	
 	
 	@Autowired	// 생성자 주입
